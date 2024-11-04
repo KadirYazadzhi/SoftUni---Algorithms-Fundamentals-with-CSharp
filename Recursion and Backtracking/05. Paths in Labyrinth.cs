616994a345994b1fb5afe5261b85ecd88814029d
Kadir Yazadzhi <smx590751@gmail.com>
2024-11-05 01:27:41 +0200

    Update exercise code

--- Recursion and Backtracking/05. Paths in Labyrinth.cs
+++ Recursion and Backtracking/05. Paths in Labyrinth.cs
@@ -1,5 +1,46 @@
-class HelloWorld {
-    static void Main() {
-        System.Console.WriteLine("give me a bottle of rum!");
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class PathsInLabyrinth {
+    static void Main(string[] args) {
+        int rows = int.Parse(Console.ReadLine());
+        int cols = int.Parse(Console.ReadLine());
+        
+        var lab = new char[rows, cols];
+
+        for (int i = 0; i < rows; i++) {
+            string line = Console.ReadLine();
+
+            for (int j = 0; j < line.Length; j++) {
+                lab[i, j] = line[j];
+            }
+        }
+        findPaths(lab, 0, 0, new List<string>(), String.Empty);
+    }
+
+    static void findPaths(char[,] lab, int rows, int cols, List<string> directions, string direction) {
+        if (rows < 0 || rows >= lab.GetLength(0) || cols < 0 || cols >= lab.GetLength(1) || lab[rows, cols] == '*' || lab[rows, cols] == '&') {
+            return;
+        }
+        
+        directions.Add(direction);
+
+        if (lab[rows, cols] == 'e') {
+            Console.WriteLine(string.Join(String.Empty, directions));
+            directions.RemoveAt(directions.Count - 1);
+            return;
+        }
+        
+        lab[rows, cols] = '&';
+        
+        findPaths(lab, rows - 1, cols, directions, "U");
+        findPaths(lab, rows + 1, cols, directions, "D");
+        findPaths(lab, rows, cols - 1, directions, "L");
+        findPaths(lab, rows, cols + 1, directions, "R");
+        
+        lab[rows, cols] = '-';
+        directions.RemoveAt(directions.Count - 1);
     }
 }
+
--- Recursion and Backtracking/06. 8 Queens Puzzle.cs
+++ Recursion and Backtracking/06. 8 Queens Puzzle.cs
@@ -1,5 +1,52 @@
-class HelloWorld {
-    static void Main() {
-        System.Console.WriteLine("give me a bottle of rum!");
+using System;
+
+public class EightQueenPuzzle {
+    public static bool[] attackedCols = new bool[8];
+    public static bool[] attackedLeftDiagonals = new bool[15];
+    public static bool[] attackedRightDiagonals = new bool[15];
+
+    static void Main(string[] args) {
+        var board = new bool[8, 8];
+        PutQueens(board, 0);
+    }
+
+    static void PutQueens(bool[,] board, int row) {
+        if (row == 8) {
+            PrintBoard(board);
+            return;
+        }
+
+        for (int col = 0; col < 8; col++) {
+            if (CanPlaceQueen(row, col)) {
+                attackedCols[col] = true;
+                attackedLeftDiagonals[row - col + 7] = true;
+                attackedRightDiagonals[row + col] = true;
+                board[row, col] = true;
+
+                PutQueens(board, row + 1);
+
+                attackedCols[col] = false;
+                attackedLeftDiagonals[row - col + 7] = false;
+                attackedRightDiagonals[row + col] = false;
+                board[row, col] = false;
+            }
+        }
+    }
+
+    private static void PrintBoard(bool[,] board) {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Console.Write(board[row, col] ? "* " : "- ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    private static bool CanPlaceQueen(int row, int col) {
+        return !attackedCols[col] && 
+               !attackedLeftDiagonals[row - col + 7] &&
+               !attackedRightDiagonals[row + col];
     }
 }
+
