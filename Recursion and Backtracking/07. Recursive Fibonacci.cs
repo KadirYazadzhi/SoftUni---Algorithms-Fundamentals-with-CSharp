fd21167893e6b166a73e6e69e0d769abb95f6227
Kadir Yazadzhi <smx590751@gmail.com>
2024-11-05 10:11:01 +0200

    Update code

--- Recursion and Backtracking/07. Recursive Fibonacci.cs
+++ Recursion and Backtracking/07. Recursive Fibonacci.cs
@@ -1,5 +1,29 @@
-class HelloWorld {
+using System;
+using System.Collections.Generic;
+
+class RecursiveFibonacci {
+    private static Dictionary<int, long> memo = new Dictionary<int, long>();
+
     static void Main() {
-        System.Console.WriteLine("give me a bottle of rum!");
+        int n = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(GetFibonacci(n));
+    }
+
+    static long GetFibonacci(int n) {
+     
+        if (n == 0 || n == 1) return 1;
+        
+        if (memo.ContainsKey(n)) {
+            return memo[n];
+        }
+        
+        long fib = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        memo[n] = fib;
+
+        return fib;
     }
 }
+
+
+
