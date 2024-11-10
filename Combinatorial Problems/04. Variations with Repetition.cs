6a62d619dd8a77660e8ba3bc6c0f56684201ce2b
Kadir Yazadzhi <smx590751@gmail.com>
2024-11-10 12:54:03 +0200

    Update 04. Variations with Repetition.cs

--- Combinatorial Problems/04. Variations with Repetition.cs
+++ Combinatorial Problems/04. Variations with Repetition.cs
@@ -1,5 +1,28 @@
-class HelloWorld {
-    static void Main() {
-        System.Console.WriteLine("give me a bottle of rum!");
+using System;
+
+class Program {
+    private static string[] elements;
+    private static int k;
+    private static string[] variations;
+
+    static void Main(string[] args) {
+        elements = Console.ReadLine().Split();
+        k = int.Parse(Console.ReadLine());
+
+        variations = new string[k];
+
+        Variations(0);
+    }
+
+    private static void Variations(int index) {
+        if (index >= variations.Length) {
+            Console.WriteLine(string.Join(" ", variations));
+            return;
+        }
+
+        for (int i = 0; i < elements.Length; i++) {
+            variations[index] = elements[i];
+            Variations(index + 1);
+        }
     }
 }
