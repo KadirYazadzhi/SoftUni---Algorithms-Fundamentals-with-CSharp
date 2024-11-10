f5aa781324e010799752672d19310d5c14c65c3e
Kadir Yazadzhi <smx590751@gmail.com>
2024-11-10 13:00:33 +0200

    Update 07. N Choose K Count.cs

--- Combinatorial Problems/07. N Choose K Count.cs
+++ Combinatorial Problems/07. N Choose K Count.cs
@@ -1,5 +1,19 @@
-class HelloWorld {
-    static void Main() {
-        System.Console.WriteLine("give me a bottle of rum!");
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(GetBinom(n, k));
     }
+
+    private static int GetBinom(int row, int col) {
+        if (row <= 1 || col == 0 || col == row) {
+            return 1;
+        }
+
+        return GetBinom(row - 1, col - 1) + GetBinom(row - 1, col);
+    }
+
 }
