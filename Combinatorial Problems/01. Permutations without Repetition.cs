7654a89d1abf15a0ce1892336e10739c973b6a60
Kadir Yazadzhi <smx590751@gmail.com>
2024-11-10 12:46:17 +0200

    Update 01. Permutations without Repetition.cs

--- Combinatorial Problems/01. Permutations without Repetition.cs
+++ Combinatorial Problems/01. Permutations without Repetition.cs
@@ -1,5 +1,32 @@
-class HelloWorld {
-    static void Main() {
-        System.Console.WriteLine("give me a bottle of rum!");
+﻿using System;
+
+
+class Program {
+    private static string[] elements;
+    private static string[] permutations;
+    private static bool[] used;
+        
+    static void Main(string[] args) {
+        elements = Console.ReadLine().Split();
+        permutations = new string[elements.Length];
+        used = new bool[elements.Length];
+
+        Permute(0);            
+    }
+
+    private static void Permute(int permutationsIndex) {
+        if (permutationsIndex >= elements.Length) {
+            Console.WriteLine(string.Join(" ", permutations));
+            return;
+        }
+
+        for (int elementsIndex = 0; elementsIndex < elements.Length; elementsIndex++) {
+            if (!used[elementsIndex]) {
+                used[elementsIndex] = true;
+                permutations[permutationsIndex] = elements[elementsIndex];
+                Permute(permutationsIndex + 1);
+                used[elementsIndex] = false;
+            }
+        }
     }
 }
