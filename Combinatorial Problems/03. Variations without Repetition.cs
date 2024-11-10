8b0be162d2288159c4c5d1638024c477cf90ff80
Kadir Yazadzhi <smx590751@gmail.com>
2024-11-10 12:49:45 +0200

    Update 03. Variations without Repetition.cs

--- Combinatorial Problems/03. Variations without Repetition.cs
+++ Combinatorial Problems/03. Variations without Repetition.cs
@@ -1,5 +1,34 @@
-class HelloWorld {
-    static void Main() {
-        System.Console.WriteLine("give me a bottle of rum!");
+﻿using System;
+
+class Program {
+    private static string[] elements;
+    private static int k;
+    private static string[] variations;
+    private static bool[] used;
+
+    static void Main(string[] args) {
+        elements = Console.ReadLine().Split();
+        k = int.Parse(Console.ReadLine());
+        
+        variations = new string[k];
+        used = new bool[elements.Length];
+
+        Variations(0);
+    }
+
+    private static void Variations(int index) {
+        if (index >= variations.Length) {
+            Console.WriteLine(string.Join(" ", variations));
+            return;
+        }
+
+        for (int i = 0; i < elements.Length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                variations[index] = elements[i];
+                Variations(index + 1);
+                used[i] = false;
+            }
+        }
     }
 }
