04448e6a29d5f8a3178bca0a1a333b39bb70754f
Kadir Yazadzhi <smx590751@gmail.com>
2024-12-05 00:04:56 +0200

    Update Binary Search.cs

--- Searching, Sorting and Greedy Algorithms/Algorithms/Binary Search.cs
+++ Searching, Sorting and Greedy Algorithms/Algorithms/Binary Search.cs
@@ -1,5 +1,44 @@
-class HelloWorld {
-    static void Main() {
-        System.Console.WriteLine("give me a bottle of rum!");
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+public class Program {
+    public static List<int> numbers = new List<int>{5, 10, 15, 30, 50, 80, 140, 150, 155, 167, 190};
+    
+    public static void Main() {
+        Console.WriteLine("Enter a number to search for: ");
+        int target = int.Parse(Console.ReadLine());
+
+        int numberIndex = BinarySearch(target);
+        if (numberIndex == -1) {
+            Console.WriteLine("No number found");
+            return;
+        }
+        
+        Console.WriteLine($"The number is found at index {numberIndex}");
+    }
+
+    private static int BinarySearch(int target) {
+        int left = 0; 
+        int right = numbers.Count - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (numbers[middle] == target) {
+                return middle;
+            }
+
+            if (target > numbers[middle]) {
+                left = middle + 1;
+                continue;
+            }
+            
+            right = middle - 1;
+        }
+
+        return -1;
     }
 }
+
